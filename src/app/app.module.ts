aff9cb1ed08980753232e6749f32527d89118bc2
haziqAhmed7 <haziq.ahmed92@gmail.com>
2020-07-18 04:16:19 +0500

    4 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,8 +5,16 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
+import {MaterialModulesAndElements} from './material.module';
+
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
+  imports:      [ 
+      BrowserModule, FormsModule,
+      BrowserAnimationsModule, HttpClientModule,
+      MaterialModulesAndElements
+    ],
   declarations: [ AppComponent, HelloComponent ],
   bootstrap:    [ AppComponent ]
 })
--- src/app/hello.component.ts
+++ src/app/hello.component.ts
@@ -1,13 +1,27 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import {CountryService} from './country.service';
 
 @Component({
   selector: 'hello',
   templateUrl: 'hello.component.html',
   styles: [`h1 { font-family: Lato; }`]
 })
-export class HelloComponent  {
+export class HelloComponent implements OnInit  {
   @Input() name: string;
   counties = [];
+  showLoading = true;
+  constructor(
+    private countryService: CountryService,
+  ) {}
 
-  
+  ngOnInit() {
+    this.loadCountries();
+  }
+
+  private async loadCountries(): Promise<object> {
+    const response = await this.countryService.GetList();
+    this.counties = (Array.isArray(response)) ? response : [];
+    return null;
+  }
+
 }
