b4f7418e947bc210794e86a4e0186a1411b2ebfa
haziqAhmed7 <haziq.ahmed92@gmail.com>
2020-07-18 03:28:48 +0500

    3 files modified, 1 added

---
+++ src/app/material.module.ts
@@ -0,0 +1,8 @@
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
+import {MatListModule} from '@angular/material/list';
+import {MatCardModule} from '@angular/material/card';
+
+export const MaterialModulesAndElements = [
+  MatProgressSpinnerModule, MatListModule,
+  MatCardModule, 
+];
